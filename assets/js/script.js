f46c5e38f2989f3f2ff0ad08952e9e01fbe7b957
ShwetaTambe0912 <shwetatambe0912@gmail.com>
2024-10-09 11:15:26 +0100

    Adding CSS file, JS file and images folder

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Window Scroll
+
+$(window).on("scroll", function() {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 80){
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header');
+    }
+});
